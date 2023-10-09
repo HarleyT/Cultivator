ee459781e8960eb27a2f26b840082332e22846e8
HarleyT <harley.trappitt@gmail.com>
2023-10-10 10:22:07 +1100

    updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var width = 1123    
-var height = 794
+var width = 1024  
+var height = 576
 var canvas = document.getElementById('Cultivator');
 var ctx = canvas.getContext('2d');
 canvas.width = width
@@ -8,7 +8,18 @@ canvas.height = height
 var image = new Image();
 image.src="./img/Cultivator_Main.png";
 
-image.onload = function() {
-    ctx.drawImage(image,0,0);
+image.onload = function() => {
+    let loadedImageWidth = image.width;
+    let loadedImageHeight = image.height;
+
+    let scale_factor = Math.min(canvas.width/image.width,canvas.height/image.height);
+
+    let newWidth = image.width * scale_factor;
+    let newHeight = image.height * scale_factor;
+    
+    let x = (canvas.width/2) - (newWidth/2);
+    let y = (canvas.height/2) - (newHeight/2);
+
+    ctx.drawImage(image, x, y, newWidth, newHeight);
     console.log('image loaded');
 }
