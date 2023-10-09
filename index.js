cbcb3ba496c3a4b3bc29a1cc598cfea2036543b1
HarleyT <harley.trappitt@gmail.com>
2023-10-10 09:28:04 +1100

    updates

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 window.addEventListener("load", () => {
-    const canvas = document.querySelector('#canvas');
-    const cxt = canvas.getContext('2d')
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d')
     const image = new Image()
+        image.src = './img/Cultivator_Main.png'
 
 });
 canvas.width = 1024
 canvas.height = 576
 
-cxt.fillStyle = 'white'
-cxt.fillRect(0, 0, canvas.width, canvas.height)
-image.src = './img/Cultivator_Main.png'
+ctx.fillStyle = 'white'
+ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 console.log(image)
